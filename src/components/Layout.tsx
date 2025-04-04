c4e7677eb6282876d328234ba628f2800402efd7
CodePuncherIbrahim <codepuncheribrahim@gmail.com>
2025-04-04 05:27:18 +0200

    added nav layout in Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { user, logout } = useAuth();
+
+  return (
+    <div className="d-flex">
+      <Navbar bg="dark" variant="dark" className="flex-column vh-100 p-3">
+        <Nav className="flex-column">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+          <Nav.Link as={Link} to="/transactions">Transactions</Nav.Link>
+          {user ? <Nav.Link onClick={logout}>Logout</Nav.Link> : null}
+        </Nav>
+      </Navbar>
+      <Container className="p-4">{children}</Container>
+    </div>
+  );
+};
+
+export default Layout;
