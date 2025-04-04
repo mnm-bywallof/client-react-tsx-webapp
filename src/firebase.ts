46741c8403a66e3e6657244d81ba5b8803eb7320
CodePuncherIbrahim <codepuncheribrahim@gmail.com>
2025-04-04 05:31:05 +0200

    added another sign-in method in firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword  } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +22,9 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export const registerWithEmail = (email: string, password: string) =>
+    createUserWithEmailAndPassword(auth, email, password);
+  
+  export const loginWithEmail = (email: string, password: string) =>
+    signInWithEmailAndPassword(auth, email, password);
